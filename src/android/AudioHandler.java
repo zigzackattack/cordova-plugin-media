91b7b7f81ab2ca33b810b46d6497be3419c63145
zigzackattack <zackbabtkis@gmail.com>
2014-04-25 16:48:40 -0700

    Fixed package name in AudioHandler.java

--- src/android/AudioHandler.java
+++ src/android/AudioHandler.java
@@ -16,7 +16,7 @@
        specific language governing permissions and limitations
        under the License.
 */
-package org.apache.cordova.riesmedia;
+package org.apache.cordova.risemedia;
 
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
